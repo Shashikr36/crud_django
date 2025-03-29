accfbd88e39fb6236cccceae87e80f864ad47b9b
Shashikr36 <shashikrpatwa36@gmail.com>
2025-03-29 11:44:00 +0530

    Update: build and poetry config changed

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-# Convert static asset files
-# python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
+poetry install --no-interaction --no-ansi
 python manage.py migrate
+python manage.py collectstatic --noinput
